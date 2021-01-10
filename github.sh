76b139224862573cf03da4257d904524ee4282a7
Nurul-GC <Nurul-GC@github.com>
2021-01-10 01:01:04 -0100

    updating commit

---
+++ github.sh
@@ -0,0 +1,7 @@
+git init
+git add ./en/*
+git add ./*
+git commit -m "updating commit"
+git remote add origin https://github.com/Nurul-GC/Guessing-Word.git
+git branch -M main
+git push -u origin main
